6ce82823a3e6d356cf01b50e56c77f84e717c652
huyenbtk <khanhhuyensthp@gmail.com>
2024-05-07 21:59:45 +0700

    update streamlit

--- app.py
+++ app.py
@@ -1,52 +1,36 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 from PIL import Image
-<<<<<<< HEAD
+import numpy as np
+import pyperclip
 
-def upload_image_page():
-    st.title('Upload Image')
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-    # File uploader widget
-    uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+local_css("style.css")
 
-    # Display the uploaded image
-    if uploaded_image is not None:
-        image = Image.open(uploaded_image)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+def extract_text(image):
+    return "Text here"
 
-def main():
-    # Nhúng file CSS bằng phương thức st.html()
-    st.markdown("""
-        <link rel="stylesheet" href="sidebar.css">
-    """, unsafe_allow_html=True)
-    st.sidebar.title("Navigation")
-    page = st.sidebar.radio("Go to", ("Home", "Upload Image", "Settings"))
+st.sidebar.title("Settings")
 
-    if page == "Home":
-        st.title("Home Page")
-        st.write("This is the home page.")
-    elif page == "Upload Image":
-        upload_image_page()
-    elif page == "Settings":
-        st.title("Settings Page")
-        st.write("This is the settings page.")
+st.title('Handwritting recognition')
 
-if __name__ == "__main__":
-    main()
-=======
-import pytesseract
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-def ocr_core(image):
-    text = pytesseract.image_to_string(image)
-    return text
-
-st.title('OCR Streamlit App')
-
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Processing...")
-    extracted_text = ocr_core(image)
-    st.write(extracted_text)
->>>>>>> ceef39bf39cd3488eb3e06aa86425a7c1a5d0f2d
+    
+    image_np = np.array(image.convert('RGB'))
+    extracted_text = extract_text(image_np)
+    
+    st.text_area("Extracted Text", extracted_text, height=150)
+    
+    # Copy text
+    if st.button('Copy Text'):
+        pyperclip.copy(extracted_text)
+        st.success("Text copied successfully!")
+else:
+    st.header("Please upload an image.")

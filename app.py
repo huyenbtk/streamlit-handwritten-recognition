bf4c8a2cd9be0248fec7ecc4de56091724aad7f9
tranduchoangnam <20febnam@gmail.com>
2024-06-08 21:58:18 +0700

    demo

--- app.py
+++ app.py
@@ -3,15 +3,114 @@ from streamlit_option_menu import option_menu
 from PIL import Image
 import numpy as np
 import pyperclip
+import tensorflow as tf
+import keras.backend as K
+from keras.models import Model
+import cv2
+from keras.layers import Input, Conv2D, MaxPooling2D, Reshape, Bidirectional, LSTM, Dense, Lambda, Activation, BatchNormalization, Dropout
+import torch 
 
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+# alphabets = u"ABCDEFGHIJKLMNOPQRSTUVWXYZ-' "
+# num_of_characters = len(alphabets) + 1 # +1 for ctc pseudo blank
 
-local_css("style.css")
+# max_str_len = 24
+# def local_css(file_name):
+#     with open(file_name) as f:
+#         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+# local_css("style.css")
+
+# def preprocess(img):
+#     (h, w) = img.shape
+    
+#     final_img = np.ones([64, 256])*255 # blank white image
+    
+#     # crop
+#     if w > 256:
+#         img = img[:, :256]
+        
+#     if h > 64:
+#         img = img[:64, :]
+    
+    
+#     final_img[:h, :w] = img
+#     return cv2.rotate(final_img, cv2.ROTATE_90_CLOCKWISE)
+
+# # the ctc loss function
+# def ctc_lambda_func(args):
+#     y_pred, labels, input_length, label_length = args
+#     # the 2 is critical here since the first couple outputs of the RNN
+#     # tend to be garbage
+#     y_pred = y_pred[:, 2:, :]
+#     return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+# def num_to_label(num):
+#     ret = ""
+#     for ch in num:
+#         if ch == -1:  # CTC Blank
+#             break
+#         else:
+#             ret+=alphabets[ch]
+#     return ret
+# input_data = Input(shape=(256, 64, 1), name='input')
+
+# inner = Conv2D(32, (3, 3), padding='same', name='conv1', kernel_initializer='he_normal')(input_data)  
+# inner = BatchNormalization()(inner)
+# inner = Activation('relu')(inner)
+# inner = MaxPooling2D(pool_size=(2, 2), name='max1')(inner)
+
+# inner = Conv2D(64, (3, 3), padding='same', name='conv2', kernel_initializer='he_normal')(inner)
+# inner = BatchNormalization()(inner)
+# inner = Activation('relu')(inner)
+# inner = MaxPooling2D(pool_size=(2, 2), name='max2')(inner)
+# inner = Dropout(0.3)(inner)
+
+# inner = Conv2D(128, (3, 3), padding='same', name='conv3', kernel_initializer='he_normal')(inner)
+# inner = BatchNormalization()(inner)
+# inner = Activation('relu')(inner)
+# inner = MaxPooling2D(pool_size=(1, 2), name='max3')(inner)
+# inner = Dropout(0.3)(inner)
+
+# # CNN to RNN
+# inner = Reshape(target_shape=((64, 1024)), name='reshape')(inner)
+# inner = Dense(64, activation='relu', kernel_initializer='he_normal', name='dense1')(inner)
+
+# ## RNN
+# inner = Bidirectional(LSTM(256, return_sequences=True), name = 'lstm1')(inner)
+# inner = Bidirectional(LSTM(256, return_sequences=True), name = 'lstm2')(inner)
+
+# ## OUTPUT
+# inner = Dense(num_of_characters, kernel_initializer='he_normal',name='dense2')(inner)
+# y_pred = Activation('softmax', name='softmax')(inner)
+
+
+# model = Model(inputs=input_data, outputs=y_pred)
+# model.summary()
+
+# # the ctc loss function
+# def ctc_lambda_func(args):
+#     y_pred, labels, input_length, label_length = args
+#     # the 2 is critical here since the first couple outputs of the RNN
+#     # tend to be garbage
+#     y_pred = y_pred[:, 2:, :]
+#     return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+
+# labels = Input(name='gtruth_labels', shape=[max_str_len], dtype='float32')
+# input_length = Input(name='input_length', shape=[1], dtype='int64')
+# label_length = Input(name='label_length', shape=[1], dtype='int64')
+
+# ctc_loss = Lambda(ctc_lambda_func, output_shape=(1,), name='ctc')([y_pred, labels, input_length, label_length])
+# model_final = Model(inputs=[input_data, labels, input_length, label_length], outputs=ctc_loss)
+# model_final.load_weights('./models/modelv1.h5')
+# model.load_weights('./models/modelv1.h5')
 def extract_text(image):
-    return "Text here"
+    image = preprocess(image)
+    image = image/255.
+    pred = model.predict(image.reshape(1, 256, 64, 1))
+    decoded = K.get_value(K.ctc_decode(pred, input_length=np.ones(pred.shape[0])*pred.shape[1], 
+                                       greedy=True)[0][0])
+    return num_to_label(decoded[0])
+
 
 st.sidebar.title("Settings")
 
@@ -22,9 +121,10 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
-    
-    image_np = np.array(image.convert('RGB'))
-    extracted_text = extract_text(image_np)
+    gray_image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2GRAY)
+
+    extracted_text = extract_text(gray_image)
+
     
     st.text_area("Extracted Text", extracted_text, height=150)
     
